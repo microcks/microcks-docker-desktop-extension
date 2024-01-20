03384d476cc8e7aa19117613d220fe1d200b0507
Hugo Guerrero <1001939+hguerrero@users.noreply.github.com>
2024-01-20 18:00:41 -0500

    Show message for unsupported bindings

--- client/src/components/MockURLRow.tsx
+++ client/src/components/MockURLRow.tsx
@@ -1,11 +1,12 @@
 import LaunchIcon from '@mui/icons-material/Launch';
+import WarningAmberIcon from '@mui/icons-material/WarningAmber';
+import Box from '@mui/material/Box';
 import IconButton from '@mui/material/IconButton';
 import Link from '@mui/material/Link';
 import Typography from '@mui/material/Typography';
 import React from 'react';
 import { useDockerDesktopClient } from '../utils/ddclient';
 import ClipboardCopy from './ClipboardCopy';
-import { Box } from '@mui/material';
 
 interface MockURLRowProps {
   bindings?: any;
@@ -21,26 +22,48 @@ const MockURLRow: React.FC<MockURLRowProps> = ({
   const ddClient = useDockerDesktopClient();
 
   return bindings ? (
-    <Box marginLeft={1} display="flex" flexDirection="column">
-      {Object.keys(bindings).map((binding: any) => (
-        <>
-          <Typography variant="body1">
-            {bindings[binding].type} endpoint:{' '}
-            <Link variant="subtitle1" underline="hover">
-              {mockURL}
-            </Link>
-            <ClipboardCopy copyText={mockURL} />
-          </Typography>
-          <Typography variant="body1">
-            {bindings[binding].type} destination:{' '}
-            <Link variant="subtitle1" underline="hover">
-            {destination}
-            </Link>
-            <ClipboardCopy copyText={destination || ''} />
-          </Typography>
-        </>
-      ))}
-    </Box>
+    <>
+      {Object.keys(bindings).map((binding: any) =>
+        binding == 'KAFKA' ? (
+          <Box
+            key={binding}
+            marginLeft={1}
+            display="flex"
+            flexDirection="column"
+          >
+            <Typography variant="body1">
+              {bindings[binding].type} endpoint:{' '}
+              <Link variant="subtitle1" underline="hover">
+                {mockURL}
+              </Link>
+              <ClipboardCopy copyText={mockURL} />
+            </Typography>
+            <Typography variant="body1">
+              {bindings[binding].type} destination:{' '}
+              <Link variant="subtitle1" underline="hover">
+                {destination}
+              </Link>
+              <ClipboardCopy copyText={destination || ''} />
+            </Typography>
+          </Box>
+        ) : (
+          <Box
+            key={binding}
+            marginLeft={1}
+            sx={{
+              display: 'flex',
+              alignItems: 'center',
+              flexWrap: 'wrap',
+            }}
+          >
+            <WarningAmberIcon />
+            <Typography variant="body1" component="span" marginLeft={1}>
+            This extension does not support the {binding} binding at this time.
+            </Typography>
+          </Box>
+        ),
+      )}
+    </>
   ) : (
     <Typography noWrap>
       <Link
